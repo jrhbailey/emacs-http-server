4a40f225c37753eb7247ef8623c03123ed9856ee
Joe Schafer <joe@jschaf.com>
2011-04-02 16:02:44 -0400

    Update copyright to GPL 3.

--- httpd.el
+++ httpd.el
@@ -1,21 +1,21 @@
 ;;; httpd.el --- HTTP/1.0 web server for emacs
 
-;; Copyright (C) 2009 Christopher Wellons <mosquitopsu@gmail.com>
+;; Copyright (C) 2011 Joe Schafer <joe@jschaf.com>
 
 ;; This file is not part of GNU Emacs.
 
-;; This file is free software; you can redistribute it and/or modify
+;; Foobar is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This file is distributed in the hope that it will be useful,
+;; Foobar is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
